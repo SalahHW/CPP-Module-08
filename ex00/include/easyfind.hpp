d6c66627bbeabd2fd45cb57861f882394732fab4
SalahHW <salah.bouheniche@gmail.com>
2024-05-31 17:19:49 +0200

    Implement easyfind function

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,13 +6,22 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 17:25:34 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/05/30 17:27:16 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/05/31 17:00:43 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
+# include <exception>
 
+template <typename T>
+void easyfind(T container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw (std::exception());
+}
 
 #endif
