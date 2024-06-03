bcd226ed0a0e9e29ada5c219313b70c514fa6914
SalahHW <salah.bouheniche@gmail.com>
2024-06-03 10:14:29 +0200

    Add ex02 empty sources files

---
+++ ex02/include/MutantStack.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/02 11:11:20 by sbouheni          #+#    #+#             */
+/*   Updated: 2024/06/02 11:18:43 by sbouheni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_HPP
+# define MUTANT_STACK_HPP
+
+# include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	
+};
+
+#endif
