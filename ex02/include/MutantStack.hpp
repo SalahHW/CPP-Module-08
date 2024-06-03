19115a4cba134ab3ec68dffd5dcb8c040a92e17b
SalahHW <salah.bouheniche@gmail.com>
2024-06-03 23:07:29 +0200

    Complete ex02

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 11:11:20 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/06/02 11:18:43 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/06/03 18:05:00 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,24 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-	
+	public:
+		~MutantStack() {};
+		MutantStack() : std::stack<T>() {};
+		MutantStack(MutantStack<T> const &other) : std::stack<T>(other) {};
+		MutantStack<T> &operator=(MutantStack<T> const &other) {
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return (*this);
+		};
+
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		iterator	begin() {
+			return (this->c.begin());
+		}
+		iterator	end() {
+			return (this->c.end());
+		}
 };
 
 #endif
--- ex02/source/main.cpp
+++ ex02/source/main.cpp
@@ -6,13 +6,59 @@
 /*   By: sbouheni <sbouheni@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 11:10:36 by sbouheni          #+#    #+#             */
-/*   Updated: 2024/06/02 11:14:04 by sbouheni         ###   ########.fr       */
+/*   Updated: 2024/06/03 18:44:02 by sbouheni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
 
-int main(void)
+int main()
 {
-	return (0);
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << std::endl;
+
+	std::list<int> mlist;
+	mlist.push_back(5);
+	mlist.push_back(17);
+	mlist.pop_back();
+	std::cout << mlist.size() << std::endl;
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+	mlist.push_back(0);
+	std::list<int>::iterator it2 = mlist.begin();
+	std::list<int>::iterator ite2 = mlist.end();
+	++it2;
+	--it2;
+	std::cout << std::endl;
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	std::list<int> s2(mlist);
+	return 0;
 }
